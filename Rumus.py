47885c5e87127ad66aa26aa663222cdc10157c0a
mandayash <amandayash20@gmail.com>
2024-06-03 00:06:58 +0700

    Luas segitiga

---
+++ Rumus.py
@@ -0,0 +1,12 @@
+# Luas segitiga
+
+
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+
+luas_segitiga()
+
